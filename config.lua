a6f39cbfcb66bba39eac964baee1404d424df0b9
Fistsofury <92045818+Fistsofury@users.noreply.github.com>
2023-09-29 21:07:08 +0100

    Update config.lua
    Initial pelt commit

--- config.lua
+++ config.lua
@@ -1,3 +1,22 @@
---[[ This is still in development
+Config = {}
 
-]]
+-- Please remember any new animals added will need to be added to the item database if not already in.
+Config.Pelts = {
+    [957520252] = { item = 'poor_bear_fur', name = 'Poor Bear Fur' },
+    [143941906] = { item = 'good_bear_fur', name = 'Good Bear Fur' },
+    [143941906] = { item = 'perfect_bear_fur', name = 'Perfect Bear Fur' },
+    [1796037447] = { item = 'poor_bighornram_hide', name = 'Poor Big Horn Ram Hide' },
+    [-476045512] = { item = 'good_bighornram_hide', name = 'Good Big Horn Ram Hide' },
+    [1795984405] = { item = 'perfect_bighornram_hide', name = 'Perfect Big Horn Ram Hide' },
+    [1248540072] = { item = 'poor_boar_skin', name = 'Poor Boar Skin' },
+    [-1858513856] = { item = 'perfect_boar_skin', name = 'Perfect Boar Skin' },
+    [1603936352] = { item = 'poor_buck_fur', name = 'Poor Buck Fur' },
+    [-868657362] = { item = 'good_buck_fur', name = 'Good Buck Fur' },
+    [-702790226] = { item = 'perfect_buck_fur', name = 'Perfect Buck Fur' },
+    [-1730060063] = { item = 'poor_bison_hide', name = 'Poor Bison Hide' },
+    [-591117838] = { item = 'good_bison_hide', name = 'Good Bison Hide' },
+    [-237756948] = { item = 'perfect_bison_hide', name = 'Perfect Bison Hide' },
+    [] = { item = '', name = '' },
+
+    -- Add more mappings for other entities here
+}
