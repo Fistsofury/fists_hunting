d094046e91281b678b01a0fb228da9b4269212aa
Fistsofury <92045818+Fistsofury@users.noreply.github.com>
2023-09-29 21:15:17 +0100

    Update sql.sql
    Updating SQL to match config.

--- sql.sql
+++ sql.sql
@@ -1,14 +1,16 @@
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_bear_fur', 'Poor Bear Fur', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('good_bear_fur', 'Good Bear Fur', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_bear_fur', 'Perfect Bear Fur', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_bighornram_hide', 'Poor Big Horn Ram Hide', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('good_bighornram_hide', 'Good Big Horn Ram Hide', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_bighornram_hide', 'Perfect Big Horn Ram Hide', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_boar_skin', 'Poor Boar Skin', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_boar_skin', 'Perfect Boar Skin', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_buck_fur', 'Poor Buck Fur', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('good_buck_fur', 'Good Buck Fur', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_buck_fur', 'Perfect Buck Fur', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_crackercow_hide', 'Poor Cracker Cow Hide', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('good_crackercow_hide', 'Good Cracker Cow Hide', -1, 1, 'item_standard', 0);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_crackercow_hide', 'Perfect Cracker Cow Hide', -1, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_bear_fur', 'Poor Bear Fur', 5, 1, 'item_standard', 0),
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('good_bear_fur', 'Good Bear Fur', 5, 1, 'item_standard', 0),
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_bear_fur', 'Perfect Bear Fur', 5, 1, 'item_standard', 0),
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_bighornram_hide', 'Poor Big Horn Ram Hide', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('good_bighornram_hide', 'Good Big Horn Ram Hide', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_bighornram_hide', 'Perfect Big Horn Ram Hide', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_boar_skin', 'Poor Boar Skin', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_boar_skin', 'Perfect Boar Skin', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_buck_fur', 'Poor Buck Fur', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('good_buck_fur', 'Good Buck Fur', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_buck_fur', 'Perfect Buck Fur', 5, 1, 'item_standard', 0);
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('poor_bison_hide', 'Poor Bison Hide', 5, 1, 'item_standard', 0),
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('good_bison_hide', 'Good Bison Hide', 5, 1, 'item_standard', 0),
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('perfect_bison_hide', 'Perfect Bison Hide', 5, 1, 'item_standard', 0);
+
+
