17f1bd921ad7d75396dbb16eb2d1a0fac99aa3ea
Fistsofury <92045818+Fistsofury@users.noreply.github.com>
2023-09-30 19:29:13 +0100

    Update config.lua
    added more animals

--- config.lua
+++ config.lua
@@ -2,21 +2,87 @@ Config = {}
 
 -- Please remember any new animals added will need to be added to the item database if not already in.
 Config.Pelts = {
+    -- Bear 
     [957520252] = { item = 'poor_bear_fur', name = 'Poor Bear Fur' },
     [143941906] = { item = 'good_bear_fur', name = 'Good Bear Fur' },
-    [1292673537 ] = { item = 'perfect_bear_fur', name = 'Perfect Bear Fur' },
+    [1292673537] = { item = 'perfect_bear_fur', name = 'Perfect Bear Fur' },
+
+    -- Big Horn Ram 
     [1796037447] = { item = 'poor_bighornram_hide', name = 'Poor Big Horn Ram Hide' },
     [-476045512] = { item = 'good_bighornram_hide', name = 'Good Big Horn Ram Hide' },
     [1795984405] = { item = 'perfect_bighornram_hide', name = 'Perfect Big Horn Ram Hide' },
+
+    -- Boar 
     [1248540072] = { item = 'poor_boar_skin', name = 'Poor Boar Skin' },
-    [-1858513856] = { item = 'perfect_boar_skin', name = 'Perfect Boar Skin' },
+    [-1858513856] = { item = 'good_boar_skin', name = 'Good Boar Skin' },
+    [279321409] = { item = 'perfect_boar_skin', name = 'Perfect Boar Skin' },
+
+    -- Buck 
     [1603936352] = { item = 'poor_buck_fur', name = 'Poor Buck Fur' },
     [-868657362] = { item = 'good_buck_fur', name = 'Good Buck Fur' },
     [-702790226] = { item = 'perfect_buck_fur', name = 'Perfect Buck Fur' },
+
+    -- Bison 
     [-1730060063] = { item = 'poor_bison_hide', name = 'Poor Bison Hide' },
     [-591117838] = { item = 'good_bison_hide', name = 'Good Bison Hide' },
     [-237756948] = { item = 'perfect_bison_hide', name = 'Perfect Bison Hide' },
+
+    -- Bull 
+    [9293261] = { item = 'poor_bull_hide', name = 'Poor Bull Hide' },
+    [-336086818] = { item = 'good_bull_hide', name = 'Good Bull Hide' },
+    [-53270317] = { item = 'perfect_bull_hide', name = 'Perfect Bull Hide' },
+
+    -- Deer 
+    [-662178186] = { item = 'poor_deer_hide', name = 'Poor Deer Hide' },
+    [-1827027577] = { item = 'good_deer_hide', name = 'Good Deer Hide' },
+    [-1035515486] = { item = 'perfect_deer_hide', name = 'Perfect Deer Hide' },
+
+    -- Elk 
+    [2053771712] = { item = 'poor_elk_fur', name = 'Poor Elk Fur' },
+    [1181652728] = { item = 'good_elk_fur', name = 'Good Elk Fur' },
+    [-1332163079] = { item = 'perfect_elk_fur', name = 'Perfect Elk Fur' },
+
+    -- American Red Fox 
+    [1647012424] = { item = 'poor_redfox_fur', name = 'Poor Red Fox Fur' },
+    [238733925] = { item = 'good_redfox_fur', name = 'Good Red Fox Fur' },
+    [500722008] = { item = 'perfect_redfox_fur', name = 'Perfect Red Fox Fur' },
+
+    -- Grey Wolf 
+    [85441452] = { item = 'poor_greywolf_fur', name = 'Poor Grey Wolf Fur' },
+    [1145777975] = { item = 'good_greywolf_fur', name = 'Good Grey Wolf Fur' },
+    [653400939] = { item = 'perfect_greywolf_fur', name = 'Perfect Grey Wolf Fur' },
+
+    -- Beaver 
+    [1104697660] = { item = 'poor_beaver_fur', name = 'Poor Beaver Fur' },
+    [1506287356] = { item = 'good_beaver_fur', name = 'Good Beaver Fur' },
+    [2117380194] = { item = 'perfect_beaver_fur', name = 'Perfect Beaver Fur' },
+
+    -- Merino Sheep 
+    [1729948479] = { item = 'poor_merinosheep_wool', name = 'Poor Merino Sheep Wool' },
+    [-1317365569] = { item = 'good_merinosheep_wool', name = 'Good Merino Sheep Wool' },
+    [1466150167] = { item = 'perfect_merinosheep_wool', name = 'Perfect Merino Sheep Wool' },
+
+    -- Pronghorn 
+    [-983605026] = { item = 'poor_pronghorn_fur', name = 'Poor Pronghorn Fur' },
+    [554578289] = { item = 'good_pronghorn_fur', name = 'Good Pronghorn Fur' },
+    [-1544126829] = { item = 'perfect_pronghorn_fur', name = 'Perfect Pronghorn Fur' },
+
+    -- Panther 
+    [1584468323] = { item = 'poor_panther_fur', name = 'Poor Panther Fur' },
+    [-395646254] = { item = 'good_panther_fur', name = 'Good Panther Fur' },
+    [1969175294] = { item = 'perfect_panther_fur', name = 'Perfect Panther Fur' },
+
+    -- Cougar 
+    [1914602340] = { item = 'poor_cougar_fur', name = 'Poor Cougar Fur' },
+    [459744337] = { item = 'good_cougar_fur', name = 'Good Cougar Fur' },
+    [-1791452194] = { item = 'perfect_cougar_fur', name = 'Perfect Cougar Fur' },
+
+    -- Coyote 
+    [-1558096473] = { item = 'poor_coyote_fur', name = 'Poor Coyote Fur' },
+    [1150939141] = { item = 'good_coyote_fur', name = 'Good Coyote Fur' },
+    [-794277189] = { item = 'perfect_coyote_fur', name = 'Perfect Coyote Fur' },
+
     --[] = { item = '', name = '' },
 
-    -- Add more mappings for other entities here
+    
 }
