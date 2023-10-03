808d8e367e5fe471b6a53b50e9140d3e254c73e3
Fistsofury <92045818+Fistsofury@users.noreply.github.com>
2023-10-03 10:46:09 +0100

    Readme & SQL update

--- README.md
+++ README.md
@@ -6,9 +6,24 @@ A hunting script that supplied pelts based on the star rating of the animal you 
 2 star = Good
 3 start = Perfect
 
-Once the animal is skinned the pelt is removed from the players hand getting rid of the annoying blip and adding the item to the players inventory.
+Once the animal is skinned the pelt is removed from the players hand adding it to their inventory
+
+Domestics animals are not included, such as cows, sheep, pigs etc.
+
+Config.lua explained.
+
+config.debug = true, This will print the hashes configured in the client lua to the console
+config.debug = false, Wont print anything.
+config.pelts, all the pelts that this script will see and delete if its in your hand and add it to the inventory, if you add more please make sure you add the additional items into your items table in your databse.
+
 
 
 To Do
--add more animals to config and SQL
+
 -add a NPC buyer of skins although configurable in most store scripts already.
+-add additional rewards for good/perfect pelts, maybe add a chance reward.
+
+Credits
+Rexshack - rsg-trapperplus, original script was inspired by a version of RSG_holding https://github.com/Rexshack-RedM/rsg-trapperplus
+Elzetia - feather-trapper & Advice  https://github.com/FeatherFramework/feather-trapper
+
--- config.lua
+++ config.lua
@@ -1,11 +1,12 @@
 Config = {}
 
--- Please remember any new animals added will need to be added to the item database if not already in.
+Config.debug = False
+
 Config.Pelts = {
     -- Bear 
-    [957520252] = { item = 'poor_bear_pelt', name = 'Poor Bear pelt' },
-    [143941906] = { item = 'good_bear_pelt', name = 'Good Bear pelt' },
-    [1292673537] = { item = 'perfect_bear_pelt', name = 'Perfect Bear pelt' },
+    {pelthash = 957520252, item = 'poor_bear_pelt', name = 'Poor Bear pelt' },
+    {pelthash = 143941906, item = 'good_bear_pelt', name = 'Good Bear pelt' },
+    {pelthash = 1292673537, item = 'perfect_bear_pelt', name = 'Perfect Bear pelt' },
 
     -- Big Horn Ram 
     {pelthash = 1796037447,  item = 'poor_bighornram_pelt', name = 'Poor Big Horn Ram pelt' },
@@ -13,74 +14,69 @@ Config.Pelts = {
     {pelthash = 1795984405, item = 'perfect_bighornram_pelt', name = 'Perfect Big Horn Ram pelt' },
 
     -- Boar 
-    [1248540072] = { item = 'poor_boar_pelt', name = 'Poor Boar pelt' },
-    [-1858513856] = { item = 'good_boar_pelt', name = 'Good Boar pelt' },
-    [279321409] = { item = 'perfect_boar_pelt', name = 'Perfect Boar pelt' },
+    {pelthash = 1292673537, item = 'poor_boar_pelt', name = 'Poor Boar pelt' },
+    {pelthash = -1858513856, item = 'good_boar_pelt', name = 'Good Boar pelt' },
+    {pelthash = 279321409, item = 'perfect_boar_pelt', name = 'Perfect Boar pelt' },
 
     -- Buck 
-    [1603936352] = { item = 'poor_buck_pelt', name = 'Poor Buck pelt' },
-    [-868657362] = { item = 'good_buck_pelt', name = 'Good Buck pelt' },
-    [-702790226] = { item = 'perfect_buck_pelt', name = 'Perfect Buck pelt' },
+    {pelthash = 1603936352, item = 'poor_buck_pelt', name = 'Poor Buck pelt' },
+    {pelthash = -868657362, item = 'good_buck_pelt', name = 'Good Buck pelt' },
+    {pelthash = -702790226, item = 'perfect_buck_pelt', name = 'Perfect Buck pelt' },
 
     -- Bison 
-    [-1730060063] = { item = 'poor_bison_pelt', name = 'Poor Bison pelt' },
-    [-591117838] = { item = 'good_bison_pelt', name = 'Good Bison pelt' },
-    [-237756948] = { item = 'perfect_bison_pelt', name = 'Perfect Bison pelt' },
+    {pelthash = -1730060063, item = 'poor_bison_pelt', name = 'Poor Bison pelt' },
+    {pelthash = -591117838, item = 'good_bison_pelt', name = 'Good Bison pelt' },
+    {pelthash = -237756948, item = 'perfect_bison_pelt', name = 'Perfect Bison pelt' },
 
     -- Bull 
-    [9293261] = { item = 'poor_bull_pelt', name = 'Poor Bull pelt' },
-    [-336086818] = { item = 'good_bull_pelt', name = 'Good Bull pelt' },
-    [-53270317] = { item = 'perfect_bull_pelt', name = 'Perfect Bull pelt' },
+    {pelthash = 9293261, item = 'poor_bull_pelt', name = 'Poor Bull pelt' },
+    {pelthash = -336086818, item = 'good_bull_pelt', name = 'Good Bull pelt' },
+    {pelthash = -53270317, item = 'perfect_bull_pelt', name = 'Perfect Bull pelt' },
 
     -- Deer 
-    [-662178186] = { item = 'poor_deer_pelt', name = 'Poor Deer pelt' },
-    [-1827027577] = { item = 'good_deer_pelt', name = 'Good Deer pelt' },
-    [-1035515486] = { item = 'perfect_deer_pelt', name = 'Perfect Deer pelt' },
+    {pelthash = -662178186, item = 'poor_deer_pelt', name = 'Poor Deer pelt' },
+    {pelthash = -1827027577, item = 'good_deer_pelt', name = 'Good Deer pelt' },
+    {pelthash = -1035515486, item = 'perfect_deer_pelt', name = 'Perfect Deer pelt' },
 
     -- Elk 
-    [2053771712] = { item = 'poor_elk_pelt', name = 'Poor Elk pelt' },
-    [1181652728] = { item = 'good_elk_pelt', name = 'Good Elk pelt' },
-    [-1332163079] = { item = 'perfect_elk_pelt', name = 'Perfect Elk pelt' },
+    {pelthash = 2053771712, item = 'poor_elk_pelt', name = 'Poor Elk pelt' },
+    {pelthash = 1181652728, item = 'good_elk_pelt', name = 'Good Elk pelt' },
+    {pelthash = -1332163079, item = 'perfect_elk_pelt', name = 'Perfect Elk pelt' },
 
     -- American Red Fox 
-    [1647012424] = { item = 'poor_redfox_pelt', name = 'Poor Red Fox pelt' },
-    [238733925] = { item = 'good_redfox_pelt', name = 'Good Red Fox pelt' },
-    [500722008] = { item = 'perfect_redfox_pelt', name = 'Perfect Red Fox pelt' },
+    {pelthash = 1647012424, item = 'poor_redfox_pelt', name = 'Poor Red Fox pelt' },
+    {pelthash = 238733925, item = 'good_redfox_pelt', name = 'Good Red Fox pelt' },
+    {pelthash = 500722008, item = 'perfect_redfox_pelt', name = 'Perfect Red Fox pelt' },
 
     -- Grey Wolf 
-    [85441452] = { item = 'poor_greywolf_pelt', name = 'Poor Grey Wolf pelt' },
-    [1145777975] = { item = 'good_greywolf_pelt', name = 'Good Grey Wolf pelt' },
-    [653400939] = { item = 'perfect_greywolf_pelt', name = 'Perfect Grey Wolf pelt' },
+    {pelthash = 85441452, item = 'poor_greywolf_pelt', name = 'Poor Grey Wolf pelt' },
+    {pelthash = 1145777975, item = 'good_greywolf_pelt', name = 'Good Grey Wolf pelt' },
+    {pelthash = 653400939, item = 'perfect_greywolf_pelt', name = 'Perfect Grey Wolf pelt' },
 
     -- Beaver 
-    [1104697660] = { item = 'poor_beaver_pelt', name = 'Poor Beaver pelt' },
-    [1506287356] = { item = 'good_beaver_pelt', name = 'Good Beaver pelt' },
-    [2117380194] = { item = 'perfect_beaver_pelt', name = 'Perfect Beaver pelt' },
-
-    -- Merino Sheep 
-    [1729948479] = { item = 'poor_merinosheep_wool', name = 'Poor Merino Sheep Wool' },
-    [-1317365569] = { item = 'good_merinosheep_wool', name = 'Good Merino Sheep Wool' },
-    [1466150167] = { item = 'perfect_merinosheep_wool', name = 'Perfect Merino Sheep Wool' },
+    {pelthash = 1104697660, item = 'poor_beaver_pelt', name = 'Poor Beaver pelt' },
+    {pelthash = 1506287356, item = 'good_beaver_pelt', name = 'Good Beaver pelt' },
+    {pelthash = 2117380194, item = 'perfect_beaver_pelt', name = 'Perfect Beaver pelt' },
 
     -- Pronghorn 
-    [-983605026] = { item = 'poor_pronghorn_pelt', name = 'Poor Pronghorn pelt' },
-    [554578289] = { item = 'good_pronghorn_pelt', name = 'Good Pronghorn pelt' },
-    [-1544126829] = { item = 'perfect_pronghorn_pelt', name = 'Perfect Pronghorn pelt' },
+    {pelthash = -983605026, item = 'poor_pronghorn_pelt', name = 'Poor Pronghorn pelt' },
+    {pelthash = 554578289, item = 'good_pronghorn_pelt', name = 'Good Pronghorn pelt' },
+    {pelthash = -1544126829, item = 'perfect_pronghorn_pelt', name = 'Perfect Pronghorn pelt' },
 
     -- Panther 
-    [1584468323] = { item = 'poor_panther_pelt', name = 'Poor Panther pelt' },
-    [-395646254] = { item = 'good_panther_pelt', name = 'Good Panther pelt' },
-    [1969175294] = { item = 'perfect_panther_pelt', name = 'Perfect Panther pelt' },
+    {pelthash = 1584468323, item = 'poor_panther_pelt', name = 'Poor Panther pelt' },
+    {pelthash = -395646254, item = 'good_panther_pelt', name = 'Good Panther pelt' },
+    {pelthash = 1969175294, item = 'perfect_panther_pelt', name = 'Perfect Panther pelt' },
 
     -- Cougar 
-    [1914602340] = { item = 'poor_cougar_pelt', name = 'Poor Cougar pelt' },
-    [459744337] = { item = 'good_cougar_pelt', name = 'Good Cougar pelt' },
-    [-1791452194] = { item = 'perfect_cougar_pelt', name = 'Perfect Cougar pelt' },
+    {pelthash = 1914602340, item = 'poor_cougar_pelt', name = 'Poor Cougar pelt' },
+    {pelthash = 459744337, item = 'good_cougar_pelt', name = 'Good Cougar pelt' },
+    {pelthash = -1791452194, item = 'perfect_cougar_pelt', name = 'Perfect Cougar pelt' },
 
     -- Coyote 
-    [-1558096473] = { item = 'poor_coyote_pelt', name = 'Poor Coyote pelt' },
-    [1150939141] = { item = 'good_coyote_pelt', name = 'Good Coyote pelt' },
-    [-794277189] = { item = 'perfect_coyote_pelt', name = 'Perfect Coyote pelt' },
+    {pelthash = -1558096473, item = 'poor_coyote_pelt', name = 'Poor Coyote pelt' },
+    {pelthash = 1150939141, item = 'good_coyote_pelt', name = 'Good Coyote pelt' },
+    {pelthash = -794277189, item = 'perfect_coyote_pelt', name = 'Perfect Coyote pelt' },
 
     --[] = { item = '', name = '' },
 
