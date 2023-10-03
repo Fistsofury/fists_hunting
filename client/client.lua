b3bd6ef30d3ad1782bb9cf1cc64dc5de0eefb750
Fistsofury <92045818+Fistsofury@users.noreply.github.com>
2023-10-03 09:59:57 +0100

    Update again
    Working version

--- client/client.lua
+++ client/client.lua
@@ -6,20 +6,55 @@ end)
 Citizen.CreateThread(function()
     while true do
         Wait(1000)
-        local entity = Citizen.InvokeNative(0xD806CD2A4F2C2996, PlayerPedId())
-        local model = GetEntityModel(entity)
-        local carriedEntityHash = Citizen.InvokeNative(0x31FEF6A20F00B963, entity)
-        local entType = GetEntityType(entity)
-        local src = source
 
-        if entType == 3 and Config.Pelts[carriedEntityHash] then
-            local peltsConfig = Config.Pelts[carriedEntityHash]
-            local item = peltsConfig.item
-            local name = peltsConfig.name
-            
-            TriggerEvent('vorp:ShowAdvancedRightNotification', name, "satchel_textures", "tick", "COLOR_PURE_WHITE", 4000)
-            TriggerServerEvent("fists_hunting:server:storepelt", item)
-            DeleteEntity(entity)
+        local ped = PlayerPedId()
+        local carriedpelt = Citizen.InvokeNative(0xD806CD2A4F2C2996, ped)
+        local pelthash = Citizen.InvokeNative(0x31FEF6A20F00B963, carriedpelt)
+
+        if Config.debug and carriedpelt then
+            print("Carrying pelt:", carriedpelt)  -- Debug print
+        end
+
+        if carriedpelt then
+            local peltsLength = #Config.Pelts
+            for i = 1, peltsLength do
+                local peltsData = Config.Pelts[i]
+
+                if pelthash == peltsData.pelthash then
+                    local name = peltsData.name
+                    local item = peltsData.item
+                    local deleted = DeleteThis(carriedpelt)
+                    
+                    if Config.debug then
+                        print("Deleted pelt:", deleted)  -- Debug print
+                    end
+                    
+                    TriggerEvent('vorp:ShowAdvancedRightNotification', name, "satchel_textures", "tick", "COLOR_PURE_WHITE", 4000)
+                    TriggerServerEvent("fists_hunting:server:storepelt", item)
+
+                    if Config.debug then
+                        print("Stored pelt item:", item)  -- Debug print
+                    end
+                end
+            end
         end
     end
 end)
+
+function DeleteThis(holding)
+    NetworkRequestControlOfEntity(holding)
+    SetEntityAsMissionEntity(holding, true, true)
+    Wait(100)
+    DeleteEntity(holding)
+    Wait(500)
+    local entitycheck = Citizen.InvokeNative(0xD806CD2A4F2C2996, PlayerPedId())
+    local holdingcheck = GetPedType(entitycheck)
+    if holdingcheck == 0 then
+        return true
+    else
+        return false
+    end
+end
+
+
+
--- config.lua
+++ config.lua
@@ -3,59 +3,59 @@ Config = {}
 -- Please remember any new animals added will need to be added to the item database if not already in.
 Config.Pelts = {
     -- Bear 
-    [957520252] = { item = 'poor_bear_fur', name = 'Poor Bear Fur' },
-    [143941906] = { item = 'good_bear_fur', name = 'Good Bear Fur' },
-    [1292673537] = { item = 'perfect_bear_fur', name = 'Perfect Bear Fur' },
+    [957520252] = { item = 'poor_bear_pelt', name = 'Poor Bear pelt' },
+    [143941906] = { item = 'good_bear_pelt', name = 'Good Bear pelt' },
+    [1292673537] = { item = 'perfect_bear_pelt', name = 'Perfect Bear pelt' },
 
     -- Big Horn Ram 
-    [1796037447] = { item = 'poor_bighornram_hide', name = 'Poor Big Horn Ram Hide' },
-    [-476045512] = { item = 'good_bighornram_hide', name = 'Good Big Horn Ram Hide' },
-    [1795984405] = { item = 'perfect_bighornram_hide', name = 'Perfect Big Horn Ram Hide' },
+    {pelthash = 1796037447,  item = 'poor_bighornram_pelt', name = 'Poor Big Horn Ram pelt' },
+    {pelthash = -476045512, item = 'good_bighornram_pelt', name = 'Good Big Horn Ram pelt' },
+    {pelthash = 1795984405, item = 'perfect_bighornram_pelt', name = 'Perfect Big Horn Ram pelt' },
 
     -- Boar 
-    [1248540072] = { item = 'poor_boar_skin', name = 'Poor Boar Skin' },
-    [-1858513856] = { item = 'good_boar_skin', name = 'Good Boar Skin' },
-    [279321409] = { item = 'perfect_boar_skin', name = 'Perfect Boar Skin' },
+    [1248540072] = { item = 'poor_boar_pelt', name = 'Poor Boar pelt' },
+    [-1858513856] = { item = 'good_boar_pelt', name = 'Good Boar pelt' },
+    [279321409] = { item = 'perfect_boar_pelt', name = 'Perfect Boar pelt' },
 
     -- Buck 
-    [1603936352] = { item = 'poor_buck_fur', name = 'Poor Buck Fur' },
-    [-868657362] = { item = 'good_buck_fur', name = 'Good Buck Fur' },
-    [-702790226] = { item = 'perfect_buck_fur', name = 'Perfect Buck Fur' },
+    [1603936352] = { item = 'poor_buck_pelt', name = 'Poor Buck pelt' },
+    [-868657362] = { item = 'good_buck_pelt', name = 'Good Buck pelt' },
+    [-702790226] = { item = 'perfect_buck_pelt', name = 'Perfect Buck pelt' },
 
     -- Bison 
-    [-1730060063] = { item = 'poor_bison_hide', name = 'Poor Bison Hide' },
-    [-591117838] = { item = 'good_bison_hide', name = 'Good Bison Hide' },
-    [-237756948] = { item = 'perfect_bison_hide', name = 'Perfect Bison Hide' },
+    [-1730060063] = { item = 'poor_bison_pelt', name = 'Poor Bison pelt' },
+    [-591117838] = { item = 'good_bison_pelt', name = 'Good Bison pelt' },
+    [-237756948] = { item = 'perfect_bison_pelt', name = 'Perfect Bison pelt' },
 
     -- Bull 
-    [9293261] = { item = 'poor_bull_hide', name = 'Poor Bull Hide' },
-    [-336086818] = { item = 'good_bull_hide', name = 'Good Bull Hide' },
-    [-53270317] = { item = 'perfect_bull_hide', name = 'Perfect Bull Hide' },
+    [9293261] = { item = 'poor_bull_pelt', name = 'Poor Bull pelt' },
+    [-336086818] = { item = 'good_bull_pelt', name = 'Good Bull pelt' },
+    [-53270317] = { item = 'perfect_bull_pelt', name = 'Perfect Bull pelt' },
 
     -- Deer 
-    [-662178186] = { item = 'poor_deer_hide', name = 'Poor Deer Hide' },
-    [-1827027577] = { item = 'good_deer_hide', name = 'Good Deer Hide' },
-    [-1035515486] = { item = 'perfect_deer_hide', name = 'Perfect Deer Hide' },
+    [-662178186] = { item = 'poor_deer_pelt', name = 'Poor Deer pelt' },
+    [-1827027577] = { item = 'good_deer_pelt', name = 'Good Deer pelt' },
+    [-1035515486] = { item = 'perfect_deer_pelt', name = 'Perfect Deer pelt' },
 
     -- Elk 
-    [2053771712] = { item = 'poor_elk_fur', name = 'Poor Elk Fur' },
-    [1181652728] = { item = 'good_elk_fur', name = 'Good Elk Fur' },
-    [-1332163079] = { item = 'perfect_elk_fur', name = 'Perfect Elk Fur' },
+    [2053771712] = { item = 'poor_elk_pelt', name = 'Poor Elk pelt' },
+    [1181652728] = { item = 'good_elk_pelt', name = 'Good Elk pelt' },
+    [-1332163079] = { item = 'perfect_elk_pelt', name = 'Perfect Elk pelt' },
 
     -- American Red Fox 
-    [1647012424] = { item = 'poor_redfox_fur', name = 'Poor Red Fox Fur' },
-    [238733925] = { item = 'good_redfox_fur', name = 'Good Red Fox Fur' },
-    [500722008] = { item = 'perfect_redfox_fur', name = 'Perfect Red Fox Fur' },
+    [1647012424] = { item = 'poor_redfox_pelt', name = 'Poor Red Fox pelt' },
+    [238733925] = { item = 'good_redfox_pelt', name = 'Good Red Fox pelt' },
+    [500722008] = { item = 'perfect_redfox_pelt', name = 'Perfect Red Fox pelt' },
 
     -- Grey Wolf 
-    [85441452] = { item = 'poor_greywolf_fur', name = 'Poor Grey Wolf Fur' },
-    [1145777975] = { item = 'good_greywolf_fur', name = 'Good Grey Wolf Fur' },
-    [653400939] = { item = 'perfect_greywolf_fur', name = 'Perfect Grey Wolf Fur' },
+    [85441452] = { item = 'poor_greywolf_pelt', name = 'Poor Grey Wolf pelt' },
+    [1145777975] = { item = 'good_greywolf_pelt', name = 'Good Grey Wolf pelt' },
+    [653400939] = { item = 'perfect_greywolf_pelt', name = 'Perfect Grey Wolf pelt' },
 
     -- Beaver 
-    [1104697660] = { item = 'poor_beaver_fur', name = 'Poor Beaver Fur' },
-    [1506287356] = { item = 'good_beaver_fur', name = 'Good Beaver Fur' },
-    [2117380194] = { item = 'perfect_beaver_fur', name = 'Perfect Beaver Fur' },
+    [1104697660] = { item = 'poor_beaver_pelt', name = 'Poor Beaver pelt' },
+    [1506287356] = { item = 'good_beaver_pelt', name = 'Good Beaver pelt' },
+    [2117380194] = { item = 'perfect_beaver_pelt', name = 'Perfect Beaver pelt' },
 
     -- Merino Sheep 
     [1729948479] = { item = 'poor_merinosheep_wool', name = 'Poor Merino Sheep Wool' },
@@ -63,24 +63,24 @@ Config.Pelts = {
     [1466150167] = { item = 'perfect_merinosheep_wool', name = 'Perfect Merino Sheep Wool' },
 
     -- Pronghorn 
-    [-983605026] = { item = 'poor_pronghorn_fur', name = 'Poor Pronghorn Fur' },
-    [554578289] = { item = 'good_pronghorn_fur', name = 'Good Pronghorn Fur' },
-    [-1544126829] = { item = 'perfect_pronghorn_fur', name = 'Perfect Pronghorn Fur' },
+    [-983605026] = { item = 'poor_pronghorn_pelt', name = 'Poor Pronghorn pelt' },
+    [554578289] = { item = 'good_pronghorn_pelt', name = 'Good Pronghorn pelt' },
+    [-1544126829] = { item = 'perfect_pronghorn_pelt', name = 'Perfect Pronghorn pelt' },
 
     -- Panther 
-    [1584468323] = { item = 'poor_panther_fur', name = 'Poor Panther Fur' },
-    [-395646254] = { item = 'good_panther_fur', name = 'Good Panther Fur' },
-    [1969175294] = { item = 'perfect_panther_fur', name = 'Perfect Panther Fur' },
+    [1584468323] = { item = 'poor_panther_pelt', name = 'Poor Panther pelt' },
+    [-395646254] = { item = 'good_panther_pelt', name = 'Good Panther pelt' },
+    [1969175294] = { item = 'perfect_panther_pelt', name = 'Perfect Panther pelt' },
 
     -- Cougar 
-    [1914602340] = { item = 'poor_cougar_fur', name = 'Poor Cougar Fur' },
-    [459744337] = { item = 'good_cougar_fur', name = 'Good Cougar Fur' },
-    [-1791452194] = { item = 'perfect_cougar_fur', name = 'Perfect Cougar Fur' },
+    [1914602340] = { item = 'poor_cougar_pelt', name = 'Poor Cougar pelt' },
+    [459744337] = { item = 'good_cougar_pelt', name = 'Good Cougar pelt' },
+    [-1791452194] = { item = 'perfect_cougar_pelt', name = 'Perfect Cougar pelt' },
 
     -- Coyote 
-    [-1558096473] = { item = 'poor_coyote_fur', name = 'Poor Coyote Fur' },
-    [1150939141] = { item = 'good_coyote_fur', name = 'Good Coyote Fur' },
-    [-794277189] = { item = 'perfect_coyote_fur', name = 'Perfect Coyote Fur' },
+    [-1558096473] = { item = 'poor_coyote_pelt', name = 'Poor Coyote pelt' },
+    [1150939141] = { item = 'good_coyote_pelt', name = 'Good Coyote pelt' },
+    [-794277189] = { item = 'perfect_coyote_pelt', name = 'Perfect Coyote pelt' },
 
     --[] = { item = '', name = '' },
 
